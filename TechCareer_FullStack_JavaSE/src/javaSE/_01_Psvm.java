53ed941be1b090c1cc0e82cca00497f1f96f94b6
iremnazyolcuu <iremnazyolcu@gmail.com>
2023-08-14 13:25:24 +0300

    Techcareer Full Stack Developer Bootcamp - Java SE

--- TechCareer_FullStack_JavaSE/src/javaSE/_01_Psvm.java
+++ TechCareer_FullStack_JavaSE/src/javaSE/_01_Psvm.java
@@ -3,6 +3,7 @@ package javaSE;
 public class _01_Psvm {
 
     public static void main(String[] args) {
-        System.out.println("Merhabalar");
+        System.out.println("Hello");
     }
+
 }
---
+++ TechCareer_FullStack_JavaSE/src/javaSE/_02_Variables.java
@@ -0,0 +1,22 @@
+package javaSE;
+
+public class _02_Variables {
+
+    public static void main(String[] args) {
+        // primitive type
+        byte b1 = 50;
+        short s1 = 128;
+        int i1 = 4554;
+        long l1 = 441441414L;
+
+        float f2 = 14.53f;
+        double d2 = 152222.555;
+
+        boolean b3 = true;
+        char c3 = 'a';
+        System.out.println(c3);
+
+        byte $_numberValue44 = 14;
+    }
+
+}
