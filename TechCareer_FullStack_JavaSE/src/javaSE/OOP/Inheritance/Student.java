1e8f228789430367aaf258326309feb018708ab6
iremnazyolcuu <iremnazyolcu@gmail.com>
2023-08-17 13:46:48 +0300

    Techcareer Full Stack Developer Bootcamp - Java SE

---
+++ TechCareer_FullStack_JavaSE/src/javaSE/OOP/Inheritance/Student.java
@@ -0,0 +1,9 @@
+package javaSE.OOP.Inheritance;
+
+import java.io.Serializable;
+
+// POJO
+// POJO+constructor: BEAN
+public class Student extends Person {
+
+}
---
+++ TechCareer_FullStack_JavaSE/src/javaSE/OOP/Interface/ICrud.java
@@ -0,0 +1,12 @@
+package javaSE.OOP.Interface;
+
+public interface ICrud {
+
+    public void create();
+
+    public void list();
+
+    public void update();
+
+    public void delete();
+}
