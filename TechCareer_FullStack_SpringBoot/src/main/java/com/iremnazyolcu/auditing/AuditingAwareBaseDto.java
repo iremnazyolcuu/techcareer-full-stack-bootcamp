f9ba0960e42500f4c79fe8b1159669777cc709e2
iremnazyolcuu <iremnazyolcu@gmail.com>
2023-08-23 18:52:14 +0300

    BlogDto

---
+++ TechCareer_FullStack_SpringBoot/src/main/java/com/iremnazyolcu/auditing/AuditingAwareBaseDto.java
@@ -0,0 +1,25 @@
+package com.iremnazyolcu.auditing;
+
+import lombok.Builder;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.util.Date;
+
+// LOMBOK
+@Getter
+@Setter
+abstract public class AuditingAwareBaseDto implements Serializable {
+    // Serialization
+    public static final Long serialVersionUID = 1L;
+
+    // ID
+    private Long id;
+
+    // DATE
+    @Builder.Default
+    private Date systemDate = new Date(System.currentTimeMillis());
+
+}
---
+++ TechCareer_FullStack_SpringBoot/src/main/java/com/iremnazyolcu/business/dto/CategoryDto.java
@@ -0,0 +1,32 @@
+package com.iremnazyolcu.business.dto;
+
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+
+import java.io.Serializable;
+
+// LOMBOK
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Log4j2
+// Category (1) - Blog (N)
+public class CategoryDto implements Serializable {
+
+    // Serialization
+    public static final Long serialVersionUID = 1L;
+
+    // CategoryDto attributes:
+
+    // CATEGORY
+    @NotEmpty(message = "{blog.category.validation.constraints.NotNull.message}")
+    @Size(min = 10, message = "{blog.category.least.validation.constraints.NotNull.message}")
+    private String categoryName;
+
+}
