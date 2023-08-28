9844671207d32cda610ddaa65f9767f17041033e
iremnazyolcuu <iremnazyolcu@gmail.com>
2023-08-28 19:47:04 +0300

    update

--- TechCareer_FullStack_SpringBoot/src/main/java/com/hamitmizrak/business/services/IEmailAttachment.java
+++
@@ -1,9 +0,0 @@
-package com.hamitmizrak.business.services;
-
-import com.hamitmizrak.business.dto.EmailDto;
-
-public interface IEmailAttachment  {
-
-    public EmailDto basicSendEmail(EmailDto emailDto);
-    public EmailDto intermediaSendEmail(EmailDto emailDto);
-}
