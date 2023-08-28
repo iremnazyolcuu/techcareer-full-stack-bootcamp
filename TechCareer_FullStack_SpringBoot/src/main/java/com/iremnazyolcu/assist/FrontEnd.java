b0697a92f8ceed98c33e9d13790f902b2648444a
iremnazyolcuu <iremnazyolcu@gmail.com>
2023-08-28 12:59:50 +0300

    updating

--- TechCareer_FullStack_SpringBoot/src/main/java/com/iremnazyolcu/assist/FrontEnd.java
+++
@@ -1,5 +0,0 @@
-package com.iremnazyolcu.assist;
-
-public class FrontEnd {
-    public static final String REACT_URL = "http://localhost:3000";
-}
--- TechCareer_FullStack_SpringBoot/src/main/java/com/iremnazyolcu/exception/IremnazYolcuException.java
+++
@@ -1,7 +0,0 @@
-package com.iremnazyolcu.exception;
-
-public class IremnazYolcuException extends RuntimeException {
-    public IremnazYolcuException(String message) {
-        super(message);
-    }
-}
