796612a76ef954d07ce6f0f7c6247a2fa41ec661
iremnazyolcuu <iremnazyolcu@gmail.com>
2023-08-28 19:49:28 +0300

    exception

---
+++ TechCareer_FullStack_SpringBoot/src/main/java/com/iremnazyolcu/exception/HamitMizrakException.java
@@ -0,0 +1,8 @@
+package com.iremnazyolcu.exception;
+
+public class HamitMizrakException extends RuntimeException{
+
+    public HamitMizrakException(String message) {
+        super(message);
+    }
+}
