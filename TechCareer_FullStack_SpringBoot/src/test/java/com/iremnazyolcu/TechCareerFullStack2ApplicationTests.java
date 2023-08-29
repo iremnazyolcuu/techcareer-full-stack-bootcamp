8e95d761ba31bdd054ffb26d171e3c0f5e03cdd2
iremnazyolcuu <iremnazyolcu@gmail.com>
2023-08-29 20:57:18 +0300

    Create TechCareerFullStack2ApplicationTests.java

---
+++ TechCareer_FullStack_SpringBoot/src/test/java/com/iremnazyolcu/TechCareerFullStack2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iremnazyolcu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechCareerFullStack2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
