9f409e665700bfcc1c461b78355980f06ec86356
iremnazyolcuu <iremnazyolcu@gmail.com>
2023-08-29 20:56:59 +0300

    exception

---
+++ TechCareer_FullStack_SpringBoot/src/main/java/com/iremnazyolcu/exception/IremnazYolcuException.java
@@ -0,0 +1,8 @@
+package com.iremnazyolcu.exception;
+
+public class IremnazYolcuException extends RuntimeException{
+
+    public IremnazYolcuException(String message) {
+        super(message);
+    }
+}
